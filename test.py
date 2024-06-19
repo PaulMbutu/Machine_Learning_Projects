7373bb3940300fd03f86c410fac1515981f32b1b
PaulMbutu <pmbutum24@gmail.com>
2024-06-19 12:18:02 +0300

    added perfomanc measures using confusion matrix

---
+++ test.py
@@ -0,0 +1,4 @@
+HOUSING_PATH = f"C:\\Users\\manch\\OneDrive\\Documents\\DEV\\MachineLearnig\\datasets\\Housing_Data\\housing.csv"
+import pandas as pd
+df=pd.read_csv(HOUSING_PATH)
+print(df.head())
