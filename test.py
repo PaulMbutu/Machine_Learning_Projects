8a5bb61bea3b2feca39c4d3184a593751bd60b91
PaulMbutu <pmbutum24@gmail.com>
2024-08-16 03:17:12 +0300

    added object detection script

--- test.py
+++ test.py
@@ -1,4 +1,8 @@
 HOUSING_PATH = f"C:\\Users\\manch\\OneDrive\\Documents\\DEV\\MachineLearnig\\datasets\\Housing_Data\\housing.csv"
 import pandas as pd
-df=pd.read_csv(HOUSING_PATH)
-print(df.head())
+#df=pd.read_csv(HOUSING_PATH)
+#print(df.head())
+import os
+
+print("yolov3.cfg exists:", os.path.exists("yolov3.cfg"))
+print("yolov3.weights exists:", os.path.exists("yolov3.weights"))
